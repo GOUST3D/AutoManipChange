fe882795f56e80bbd8a39ecbca45ed17dd4fc1f1
GOUST3D <79782477+GOUST3D@users.noreply.github.com>
2021-09-15 22:08:10 -0700

    Update AutoManipChange_start.py

--- AutoManipChange_start.py
+++ AutoManipChange_start.py
@@ -1,19 +1,23 @@
 import maya.cmds as cmds
 import maya.mel as mel
 
+try:
+	cmds.scriptJob(kill=AutoManipChangeScriptJob)
+except:
+	pass
 
 class AutoManipChange():
-    def __init__(self):
-        selected = cmds.ls(sl=True)[0]
-        print "sucka"
-        if cmds.getAttr(str(selected)+".showManipDefault") == 1:
-            mel.eval("MoveTool;")
-            return
-        if cmds.getAttr(str(selected)+".showManipDefault") == 2:
-            mel.eval("RotateTool;")
-            return
-        if cmds.getAttr(str(selected)+".showManipDefault") == 3:
-            mel.eval("ScaleTool;")
-            return
+	def __init__(self):
+		selected = cmds.ls(sl=True)[0]
+		print "sucka"
+		if cmds.getAttr(str(selected)+".showManipDefault") == 1:
+			mel.eval("MoveTool;")
+			return
+		if cmds.getAttr(str(selected)+".showManipDefault") == 2:
+			mel.eval("RotateTool;")
+			return
+		if cmds.getAttr(str(selected)+".showManipDefault") == 3:
+			mel.eval("ScaleTool;")
+			return
 
 AutoManipChangeScriptJob = cmds.scriptJob( ct= ["SelectionChanged","AutoManipChange()"], protected=True, kws=True)
