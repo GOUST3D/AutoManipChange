0a8d2e915954f16391b3c092d28a52cee0b1b5a7
GOUST3D <79782477+GOUST3D@users.noreply.github.com>
2021-09-15 21:59:26 -0700

    Create AutoManipChange_start.py

---
+++ AutoManipChange_start.py
@@ -0,0 +1,19 @@
+import maya.cmds as cmds
+import maya.mel as mel
+
+
+class AutoManipChange():
+    def __init__(self):
+        selected = cmds.ls(sl=True)[0]
+        print "sucka"
+        if cmds.getAttr(str(selected)+".showManipDefault") == 1:
+            mel.eval("MoveTool;")
+            return
+        if cmds.getAttr(str(selected)+".showManipDefault") == 2:
+            mel.eval("RotateTool;")
+            return
+        if cmds.getAttr(str(selected)+".showManipDefault") == 3:
+            mel.eval("ScaleTool;")
+            return
+
+AutoManipChangeScriptJob = cmds.scriptJob( ct= ["SomethingSelected","AutoManipChange()"], protected=True, kws=True)
